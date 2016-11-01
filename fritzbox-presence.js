697b1eb85b73bf6445469f01f18c1a0137e6ddba
Felix Fischer <felix@subtract.de>
2016-11-01 20:23:14 +0100

    code refactoring

--- fritzbox-presence.js
+++ fritzbox-presence.js
@@ -15,31 +15,33 @@ module.exports = function(RED) {
         function getDevices(sid) {
             if (sid == '0000000000000000') {
                 node.status({ fill: "red", shape: "dot", text: "connection error" })
-                node.error('could not get a valid session ID. Please check credentials.')
-                return
+                return node.error('could not get a valid session ID. Please check credentials.')
             }
             sid = sid || sessionID
+            if (sid == '' || typeof sid != 'string') {
+                node.status({ fill: "green", shape: "ring", text: "connecting" })
+                return fritz.getSessionID(username, password, getDevices)
+            }
             fritz.checkSession(sid, function(isSession) {
-                if (isSession) {
-                    sessionID = sid
-                    node.status({ fill: "green", shape: "dot", text: "querying" })
-                    fritz.getData(sid, function(data) {
-                        data = JSON.parse(data)
-                        var devices = data.data.active
-                        node.send({ payload: data.data.active })
-                        node.status({ fill: "green", shape: "ring", text: `${devices.length} devices detected` })
-                    })
-                } else {
-                    node.status({ fill: "green", shape: "ring", text: "connecting" })
-                    fritz.getSessionID(username, password, getDevices)
+                if (!isSession) {
+                    return getDevices()
                 }
+                sessionID = sid
+                node.status({ fill: "green", shape: "dot", text: "querying" })
+                fritz.getData(sid, function(res) {
+                    try {
+                        var devices = JSON.parse(res).data.active
+                        node.status({ fill: "green", shape: "ring", text: `${devices.length} devices detected` })
+                        node.send({ payload: devices })
+                    } catch (e) {
+                        node.status({ fill: "red", shape: "dot", text: "data error" })
+                        node.error(e)
+                    }
+                })
             })
         }
 
-        this.on('input', function(msg) {
-            node.status({ fill: "yellow", shape: "dot", text: "starting" })
-            getDevices()
-        })
+        this.on('input', getDevices)
 
         this.on('close', function() {
             node.status({})
